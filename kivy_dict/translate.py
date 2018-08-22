2fe0b02cc465b522ffe8fe311a0a2cece3094bdf
Yao <yao@cryptape.com>
2018-08-22 21:02:54 +0800

    update: add download audio func

--- kivy_dict/translate.py
+++ kivy_dict/translate.py
@@ -1,8 +1,11 @@
 import json
 import urllib.request
+import sys
 
 
 log = print
+
+key = '5ECCF2BFE29482607F1970245FD41519'
 
 
 def openurl(url):
@@ -23,14 +26,51 @@ def means(parts):
     return s
 
 
-def translate(word):
-    key = '5ECCF2BFE29482607F1970245FD41519'
+def fetch_info(word):
     url = 'http://dict-co.iciba.com/api/dictionary.php?type=json&key={}&w={}'.format(
         key, word)
     string = openurl(url)
-    parts = json.loads(string)['symbols'][0]['parts']
+    info = json.loads(string)
+    null = {'symbols': [
+        {'ph_am_mp3': '', 'ph_en_mp3': '', 'ph_other': '', 'ph_tts_mp3': ''}]}
+    if info == null:
+        return None
+    return info
+
+
+def translate(info):
+    if info == None:
+        return
+    parts = info['symbols'][0]['parts']
     s = means(parts)
     return s
+
+
+def download(url, name):
+    path = audio_path(name)
+    s = urllib.request.urlopen(url).read()
+    # 'w' 表示写入  'b' 表示二进制模式
+    with open(path, 'wb') as f:
+        f.write(s)
+    return path
+
+
+def audio_path(name):
+    root = sys.path[0]
+    path = '{}/audio/{}.mp3'.format(root, name)
+    return path
+
+
+def download_audio(info, name):
+    if info == None:
+        return
+    ph_am_mp3 = info['symbols'][0]['ph_am_mp3']
+    log(ph_am_mp3)
+    if ph_am_mp3 == '':
+        return None
+    url = '{}?key={}'.format(ph_am_mp3, key)
+    path = download(url, name)
+    return path
 
 
 def main():
